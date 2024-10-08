83292fe33a4d8b37ccca5701f48007804f311370
Matthew Lazar <matthewlazar@Matthews-Air.modem>
2024-10-08 18:59:34 +1100

    set up the main file

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+import numpy as np
+import math
+
+def main():
+    # todo:
+    return
+
+if __name__ == "__main__":
+    main()
